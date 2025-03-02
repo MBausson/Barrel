1fd4022af33c63448668e03de419945a85b140e0
MBausson <maximilienbausson@icloud.com>
2025-03-02 17:41:30 +0100

    Added reccurent schedule options & .Every & tests

---
+++ BarrelTest/Units/RecurrentScheduleOptionsTest.cs
@@ -0,0 +1,25 @@
+﻿namespace BarrelTest.Units;
+
+public class RecurrentScheduleOptionsTest
+{
+    private readonly RecurrentScheduleOptions _options = new();
+
+    [Fact]
+    public void Every_SetsPeriodicityTest()
+    {
+        var everySeconds = Random.Shared.Next(1, 3600);
+
+        _options.Every(TimeSpan.FromSeconds(everySeconds));
+
+        Assert.Equal(everySeconds, _options.Periodicity.TotalSeconds);
+    }
+
+    [Fact]
+    public void Every_TooShortDelayTest()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+        {
+            _options.Every(TimeSpan.FromMilliseconds(999));
+        });
+    }
+}
