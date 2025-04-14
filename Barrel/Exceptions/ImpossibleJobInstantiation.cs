dc0a17b41919ad8f9edea19c041bd9f3a8cdb03b
MBausson <maximilienbausson@icloud.com>
2025-04-14 20:57:57 +0200

    wip tests

---
+++ Barrel/Exceptions/ImpossibleJobInstantiation.cs
@@ -0,0 +1,8 @@
+﻿using Barrel.JobData;
+
+namespace Barrel.Exceptions;
+
+public class ImpossibleJobInstantiation(BaseJobData jobData) : Exception(
+    $"Could not instantiate {jobData.GetType()} (job id '{jobData.JobId}') : " +
+    $"register the job via dependency injection, or provide an argument-less constructor, " +
+    $"or provide a job instance when scheduling.");
---
+++ BarrelTest/Integrations/DependencyInjectionTests.cs
@@ -0,0 +1,66 @@
+﻿using Barrel.Exceptions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BarrelTest.Integrations;
+
+public class DependencyInjectionTests(ITestOutputHelper output) : IntegrationTest(output)
+{
+    [Fact]
+    //  Ensures that we cannot schedule a job if it cannot be instantiated either via DI, or an argument-less
+    //  constructor
+    public void CannotScheduleWithoutDependencyInjection()
+    {
+        Scheduler = new JobScheduler(ConfigurationBuilder);
+
+        Assert.Throws<ImpossibleJobInstantiation>(() =>
+        {
+            Scheduler.Schedule<DependentJob>();
+        });
+    }
+
+    [Fact]
+    //  Ensures that a job gets ran when its dependencies have been declared via DI
+    public async Task RunJobWithDependencyInjectionTest()
+    {
+        var services = new ServiceCollection();
+        services.AddScoped<IDependency, Dependency>();
+        services.AddScoped<DependentJob>();
+
+        IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+        ConfigurationBuilder.WithDependencyInjection(serviceProvider);
+        Scheduler = new JobScheduler(ConfigurationBuilder);
+
+        var jobData = Scheduler.Schedule<DependentJob>();
+        var job = (DependentJob)jobData.Instance!;
+
+        await WaitForJobToEnd(job);
+
+        Assert.True(Dependency.HasWorked);
+    }
+
+    private interface IDependency
+    {
+        public void Work();
+    }
+
+    private class Dependency : IDependency
+    {
+        public static bool HasWorked { get; private set; }
+
+        public void Work()
+        {
+            HasWorked = true;
+        }
+    }
+
+    private class DependentJob(IDependency dependency) : TestJob
+    {
+        protected override Task PerformAsync()
+        {
+            dependency.Work();
+
+            return base.PerformAsync();
+        }
+    }
+}
