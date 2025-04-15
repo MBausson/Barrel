d741b4c7300201349921a65274cf42935dae14e6
MBausson <maximilienbausson@icloud.com>
2025-04-15 22:30:04 +0200

    wip

---
+++ Barrel/AnonymousJob.cs
@@ -0,0 +1,9 @@
+﻿namespace Barrel;
+
+public class AnonymousJob(Action action) : BaseJob
+{
+    protected internal override Task PerformAsync()
+    {
+        return Task.Factory.StartNew(action);
+    }
+}
